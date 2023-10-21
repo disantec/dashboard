bba6d385cdce24dc268bdeaf0c8404eabe7ffff1
disantec <disantec@gmail.com>
2023-10-21 11:35:09 -0500

    4-simplify-files: updated file structure

---
+++ TouchGFX/build/STM32F469-DISCO/Core/Src/application_main.d
@@ -0,0 +1,37 @@
+TouchGFX/build/STM32F469-DISCO/Core/Src/application_main.o: \
+ Core/Src/application_main.cpp Core/Inc/data_store.h \
+ Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os.h \
+ Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ Core/Inc/FreeRTOSConfig.h \
+ Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ gcc/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
+ Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.h
+
+Core/Inc/data_store.h:
+
+Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os.h:
+
+Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+
+Core/Inc/FreeRTOSConfig.h:
+
+Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+
+Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+
+Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+gcc/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
+
+Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.h:
