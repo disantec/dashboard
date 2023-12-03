6c3b2e6fd67e3ec12c6de5cffede06ea11537e9c
Loucka <christian.loucka@gmail.com>
2023-12-02 19:01:55 -0500

    Initial CAN test

--- Core/Src/application_main.cpp
+++ Core/Src/application_main.cpp
@@ -12,27 +12,46 @@ void application_main(void *arg, CAN_HandleTypeDef *hcan)
 {
     int rpm=1, rpmstore;
 
+    CAN_RxHeaderTypeDef RxHeader;
+    uint8_t RxData[8];
+
     
-    if(HAL_OK == HAL_CAN_Start(hcan))
+    if(HAL_OK != HAL_CAN_Start(hcan))
     {
-        data_store::instance()->set_rpm(456);
+        data_store::instance()->set_rpm(666);
     }
+    else
+    {
+        while (true) 
+        {
+            
+            /*while (rpmstore<10) {
+                rpmstore=++rpm;
 
+                osDelay(500);
 
-while (true) {
-    while (rpmstore<10) {
-        rpmstore=++rpm;
+                //data_store::instance()->set_rpm(rpmstore);
+            }
+            
+            rpm = 1;
+            rpmstore = 1;
+            
+            //data_store::instance()->set_rpm(rpmstore);*/
 
-        osDelay(500);
+            if (HAL_OK == HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData))
+            {
+                rpm = 111;
+            }
+            else
+            {
+                rpm = 222;
+            }
 
-        //data_store::instance()->set_rpm(rpmstore);
+            data_store::instance()->set_rpm(rpm);
+            
+            HAL_Delay(100);
+        }
     }
-    
-    rpm = 1;
-    rpmstore = 1;
-    
-    //data_store::instance()->set_rpm(rpmstore);
-}
 }
 
 #ifdef __cplusplus
