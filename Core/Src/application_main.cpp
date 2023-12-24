82e8a22a29c0bddc4bda6678ec6d4483c4eb3666
Loucka <christian.loucka@gmail.com>
2023-12-23 23:59:49 -0500

    Adds a test parse

--- Core/Src/application_main.cpp
+++ Core/Src/application_main.cpp
@@ -5,6 +5,8 @@
 extern "C" {
 #endif
 
+//#define TEST_MODE
+
 /// @brief Application C++ entry point. Responsible for CAN monitoring and delegation of processing.
 ///
 /// @param arg  Applicable args from C invocation. Unused
@@ -19,12 +21,19 @@ void application_main(void *arg, CAN_HandleTypeDef *hcan)
     // Begin infinite "main" loop of program. Execution is not intended to move beyond this.
     while (true) 
     {
+        #ifdef TEST_MODE
+        rxData[0] = 0x02;
+        rxData[1] = 0x01;
+
+        p_can_parser->process(0x5F0, rxData);
+        #else
         // For each main loop, scoop up all available CAN messages that have come in and forward them off for processing.
         // This processing loop will continue so long as messages are available in the FIFO.
         while (0 != HAL_CAN_GetRxFifoFillLevel(hcan, CAN_RX_FIFO0) && HAL_OK == HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &rxHeader, rxData))
         {
             p_can_parser->process(rxHeader.StdId, rxData);
         }
+        #endif
 
         // Provide a 1ms sleep to limit the MCU from running as fast as possible. 
         HAL_Delay(1);
--- Core/Src/can_parser.cpp
+++ Core/Src/can_parser.cpp
@@ -10,8 +10,8 @@ void can_parser::process(uint32_t id, uint8_t data[])
     {
         case 0x5F0:
             // RPM, Throttle Position, Ground Speed, Engine Temp
-            // Casting and endianness are probably wrong, but it's a start.
-            p_data_store_->set_rpm(data[0] && (data[1] << 16));
+            // Endianness is probably wrong, but it's a start.
+            p_data_store_->set_rpm(data[0] | (data[1] << 8));
             break;
         case 0x5F1:
             // Inlet Air Temp, Gear, Lambda 1, MAP
