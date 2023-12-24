d55dcffbc0d68e19a33890df95499a2f29c29089
Loucka <christian.loucka@gmail.com>
2023-12-23 21:11:01 -0500

    More things we don't need

--- Core/Src/application_main.cpp
+++ Core/Src/application_main.cpp
@@ -10,11 +10,8 @@ extern "C" {
 
 void application_main(void *arg, CAN_HandleTypeDef *hcan)
 {
-    CAN_TxHeaderTypeDef   TxHeader;
     CAN_RxHeaderTypeDef   RxHeader;
-    uint8_t               TxData[8];
     uint8_t               RxData[8];
-    uint32_t              TxMailbox;
 
     uint32_t count = 0;
 
@@ -25,7 +22,6 @@ void application_main(void *arg, CAN_HandleTypeDef *hcan)
         if (0 != HAL_CAN_GetRxFifoFillLevel(hcan, CAN_RX_FIFO0) && HAL_OK==HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData))
         {
             count++;
-
         }
 
         data_store::instance()->set_rpm(count);
