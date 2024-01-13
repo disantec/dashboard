3dc804b8b25073b97f8f5ba2772cd76fac6b9d89
disantec <disantec@gmail.com>
2024-01-13 13:12:02 -0600

    turned off led, switching to some printf for messages

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -27,6 +27,7 @@
 #include "../Components/nt35510/nt35510.h"
 #include "stm32469i_discovery_sdram.h"
 #include "stm32469i_discovery_qspi.h"
+#include "mpu6050.h"
 
 /* USER CODE END Includes */
 
@@ -152,6 +153,8 @@ int main(void)
   /* Call PreOsInit function */
   MX_TouchGFX_PreOSInit();
   /* USER CODE BEGIN 2 */
+
+  mpu6050_init();
 
   /* USER CODE END 2 */
 
@@ -764,7 +767,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13|GPIO_PIN_11|GPIO_PIN_10, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOG, MCU_ACTIVE_Pin|GPIO_PIN_6, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(MCU_ACTIVE_GPIO_Port, MCU_ACTIVE_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOK, GPIO_PIN_3, GPIO_PIN_SET);
@@ -781,8 +784,8 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
 
-  /*Configure GPIO pins : PG13 PG11 PG10 PG6 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_11|GPIO_PIN_10|GPIO_PIN_6;
+  /*Configure GPIO pins : PG13 PG11 PG10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_11|GPIO_PIN_10;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -848,18 +851,6 @@ static void MX_GPIO_Init(void)
 void StartDefaultTask(void *argument)
 {
   /* USER CODE BEGIN 5 */
-
-      HAL_StatusTypeDef ret = HAL_I2C_IsDeviceReady
-
-        (&hi2c1, (1101000) << 1 + 0, 1, 100);
-
-            if(ret == HAL_OK)
-
-                HAL_GPIO_WritePin (GPIOG, GPIO_PIN_6, GPIO_PIN_SET);
-
-            else
-
-                HAL_GPIO_WritePin (GPIOG, GPIO_PIN_6, GPIO_PIN_SET); 
 
   if (HAL_CAN_Start(&hcan2) != HAL_OK) { Error_Handler(); }
   else                                 { application_main(argument, &hcan2, &hi2c1); }
