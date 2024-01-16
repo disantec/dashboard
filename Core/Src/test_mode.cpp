8bf252e9dcaa2d3dbdcac5ce24c8645f6eb6c657
Loucka <christian.loucka@gmail.com>
2024-01-15 21:24:36 -0500

    Refactores get_rx_message

--- Core/Src/test_mode.cpp
+++ Core/Src/test_mode.cpp
@@ -6,35 +6,22 @@ test_mode *test_mode::p_instance_ = nullptr;
 
 bool test_mode::get_rx_message(CAN_RxHeaderTypeDef *pHeader, uint8_t data[])
 {
-    ///@todo Clean this up, time permitting.
-    if (burst_active_)
+    if (burst_active_ && MESSAGE_BURST_COUNT > burst_count_)
     {
-        if (MESSAGE_BURST_COUNT > burst_count_)
-        {
-            populate_message(pHeader, data);
-            burst_count_++;
-            return true;
-        }
-        else
-        {
-            burst_active_ = false;
-            burst_count_ = 0;
-            return get_rx_message(pHeader, data);
-        }
+        populate_message(pHeader, data);
+        burst_count_++;
+        return true;
+    }
+    else if (!burst_active_ && MESSAGE_BURST_DELAY > burst_count_)
+    {
+        burst_count_++;
+        return false;
     }
     else
     {
-        if (MESSAGE_BURST_DELAY > burst_count_)
-        {
-            burst_count_++;
-            return false;
-        }
-        else
-        {
-            burst_active_ = true;
-            burst_count_ = 0;
-            return get_rx_message(pHeader, data);
-        }
+        burst_active_ = !burst_active_;
+        burst_count_ = 0;
+        return get_rx_message(pHeader, data);
     }
 }
 
